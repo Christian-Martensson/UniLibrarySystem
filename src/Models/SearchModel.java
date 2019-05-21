c89e4aa125a468df043eab0f9eccc78ef6adf2a6
christianmartensson <christian.martonens@gmail.com>
2019-05-21 12:23:57 +0200

    Now possible to search for author and title at the same time.

--- src/Models/SearchModel.java
+++ src/Models/SearchModel.java
@@ -55,8 +55,16 @@ public class SearchModel {
             connection = driver.createConnection();
 
             // 2. Create a statement
-            PreparedStatement statement = connection.prepareStatement("SELECT * FROM Book WHERE Match(title) Against(?)");
+            // previous: "SELECT * FROM Book WHERE Match(title) Against(?)"
+            String sqlQuery = "SELECT b.*, c.* \n" +
+                    "FROM Book b\n" +
+                    "JOIN BookAuthor ba on b.isbn = ba.isbn\n" +
+                    "JOIN Creator c on c.creatorId = ba.authorId\n" +
+                    "WHERE Match(b.title) Against(?)\n" +
+                    "OR Match(c.fName) Against(?);\n";
+            PreparedStatement statement = connection.prepareStatement(sqlQuery);
             statement.setString(1, searchWord);
+            statement.setString(2, searchWord);
 
             // 3. Execute SQL query
             ResultSet resultSet = statement.executeQuery();
@@ -75,6 +83,9 @@ public class SearchModel {
                 BookModel book = new BookModel(isbn, articleType, title, publisher, genre, publicationYear);
                 listOfBooks.add(book);
             }
+
+
+
         }
 
         catch (Exception e) {
