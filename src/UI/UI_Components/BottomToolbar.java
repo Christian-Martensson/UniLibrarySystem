f659a28597b214b7f74a3eefd796866eb8745210
Josefine Gustafsson <josefine.bl.gustafsson@hotmail.com>
2019-05-22 09:50:34 +0200

    Changed some of the names to Database names, eg. First Name Tried to enter more labels and fields but couldnt get i right according to the GridBagLayout..

--- src/UI/UI_Components/BottomToolbar.java
+++ src/UI/UI_Components/BottomToolbar.java
@@ -8,6 +8,8 @@ import java.awt.event.ActionListener;
 public class BottomToolbar extends JPanel {
     private JButton borrowButton;
     private JButton reserveButton;
+    private JButton editButton;
+    private JButton deleteButton;
 
 
 
@@ -16,22 +18,35 @@ public class BottomToolbar extends JPanel {
 
         borrowButton = new JButton("Borrow");
         reserveButton = new JButton("Reserve");
+        editButton = new JButton("Edit");
+        deleteButton = new JButton("Delete");
 
 
         setLayout(new FlowLayout(FlowLayout.LEFT));
 
         add(borrowButton);
         add(reserveButton);
+        add(editButton);
+        add(deleteButton);
 
     }
 
-    /*public void addSearchButtonListener (ActionListener listenForSearchButton) {
+    /*
+    public void addReserveButtonListener (ActionListener listenForSearchButton) {
         reserveButton.addActionListener(listenForSearchButton);
     }
 
-    public void addLoginButtonListener (ActionListener listenForLoginButton) {
+    public void addBorrowButtonListener (ActionListener listenForLoginButton) {
         borrowButton.addActionListener(listenForLoginButton);
-    }*/
+    }
 
+    public void addEditButtonListener (ActionListener listenForEditButton) {
+        editButton.addActionListener(listenForEditButton);
+    }
+
+    public void addDeleteButtonListener (ActionListener listenForDeleteButton) {
+        deleteButton.addActionListener(listenForDeleteButton);
+    }
+    */
 
 }
